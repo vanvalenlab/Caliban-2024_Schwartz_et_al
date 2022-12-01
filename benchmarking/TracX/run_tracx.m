fc265e31f685234a11e978f5ad3e5ca7fe961a3a
Morgan Schwartz <msschwartz21@gmail.com>
2022-11-30 20:41:33 -0800

    Benchmark against TracX (#8)
    * Basic setup for TracX benchmarking
    
    * Complete draft of the tracx benchmarking routine
    
    * Run complete Tracx benchmarking
    
    * Remove extra cell with notes
    
    * Change naming in embedtrack benchmark csv
    
    * Update figures with results from tracx

---
+++ benchmarking/TracX/run_tracx.m
@@ -0,0 +1,46 @@
+dirs = ["seg-gt", "seg-dc"]
+ids = ["001", "002", "003", "004", "005", "006", "007", "008", "009", "010", "011", "012"]
+lens = [42, 42, 42, 42, 42, 50, 50, 71, 71, 65, 45, 55]
+
+n = length(dirs)
+for i=1:1:n
+    d = convertStringsToChars(dirs(i))
+    
+    m = length(lens)
+    for j=1:1:m
+        idx = convertStringsToChars(ids(j))
+        movieLength = lens(j)
+
+        % Prep segmentations
+        Tracker = TracX.Tracker();
+        imagePath = fullfile('data', 'raw', convertStringsToChars(idx));
+        segmentationPath = fullfile('data', d, convertStringsToChars(idx));
+        segmentationFileNameRegex = 'mask*.tif';
+        imageFileNameRegex = 'nuclear*.tif';
+        Tracker.prepareDataFromSegmentationMask(imagePath, ...
+            segmentationPath, segmentationFileNameRegex, ...
+            imageFileNameRegex);
+        clear Tracker
+
+        % Configure a tracking project
+        projectName = d + idx;
+        fileIdentifierFingerprintImages = 'nuclear';
+        fileIdentifierWellPositionFingerprint = [];
+        fileIdentifierCellLineage = '';
+        imageCropCoordinateArray = [];
+        cellDivisionType = 'sym';
+
+        % Create a tracker instance and a new project
+        Tracker = TracX.Tracker();
+        Tracker.createNewTrackingProject(projectName, imagePath, ...
+                segmentationPath, fileIdentifierFingerprintImages, ...
+                fileIdentifierWellPositionFingerprint, fileIdentifierCellLineage, ...
+                imageCropCoordinateArray, movieLength, cellDivisionType);
+
+        Tracker.runTracker()
+        Tracker.runLineageReconstruction('symmetricalDivision', true);
+
+        Tracker.saveTrackerCellCycleResultsAsTable()
+        Tracker.saveTrackerResultsAsTable()
+    end
+end
